eeca75bb70a412d8ba2e3e87c363cbdc2a2f6578
iamjongholee <jongho0530@gmail.com>
2022-02-22 01:39:56 +0900

    commit on 'main'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let [글제목, 글제목변경] = useState([
+    "프로필 수정",
+    "환경설정",
+    "보드 관리",
+    "다이어리 관리",
+  ]);
+  let [like, likechange] = useState(0);
+
+  function 제목바꾸기(){
+    var newArray = [...글제목];
+    newArray[0] = '제목 바꿔보기';
+    글제목변경( newArray );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">Hello Blog!</div>
+      <button onClick={제목바꾸기}>버튼을 클릭하세요</button>
+      <div className="list">
+        <h3>
+          {글제목[0]}{" "}
+          <span onClick={() => { likechange(like + 1); }}>💖</span>{" "}{like}{" "}
+        </h3>
+        <p>내용1</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3>{글제목[1]}</h3>
+        <p>내용2</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3>{글제목[2]}</h3>
+        <p>내용3</p>
+        <hr />
+      </div>
+      <Modal></Modal>
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
